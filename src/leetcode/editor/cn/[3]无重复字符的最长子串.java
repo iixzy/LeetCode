e3a7d6b04a08621b4439fca7d7c1de269575ba45
wangsc <wsc4007@gmail.com>
2020-11-27 15:40:36 +0800

    2020.11.27

---
+++ src/leetcode/editor/cn/[3]无重复字符的最长子串.java
@@ -0,0 +1,112 @@
+package leetcode.editor.cn;
+
+//给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "abcabcbb"
+//输出: 3 
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: s = "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+// 
+//
+// 示例 4: 
+//
+// 
+//输入: s = ""
+//输出: 0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length <= 5 * 104 
+// s 由英文字母、数字、符号和空格组成 
+// 
+// Related Topics 哈希表 双指针 字符串 Sliding Window 
+// 👍 4634 👎 0
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class 无重复字符的最长子串{
+	public static void main(String[] args) {
+		Solution solution = new 无重复字符的最长子串().new Solution();
+		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+//		暴力法
+//    	int n = s.length();
+//		int max = 0;
+//		Set<Character> set = new HashSet<>();
+//		for (int i = 0; i < n; i++) {
+//			int count = 0;
+//			for (int j = i; j < n; j++) {
+//				if (set.add(s.charAt(j))) {//有一个不相同,次数就加一
+//					count++;
+//					max = Math.max(max, count);//置换最大值
+//				} else {
+//					set.clear();
+//					break;
+//				}
+//			}
+//		}
+//		return max;
+
+
+		/**
+		 * 以这个字符串为例：abcabcbb，当i等于3时，也就是指向了第二个a, 此时我就需要查之前有没有出现过a,
+		 * 如果出现了是在哪一个位置出现的。然后通过last[index] 查到等于1, 也就是说，如果start 依然等于0的话，
+		 * 那么当前窗口就有两个a了，也就是字符串重复了，所以我们需要移动当前窗口的start指针，移动到什么地方呢？
+		 * 移动到什么地方，窗口内就没有重复元素了呢？ 对了，就是a上一次出现的位置的下一个位置，就是1 + 1 = 2。
+		 * 当start == 2, 当前窗口就没有了重复元素，那么以当前字符为结尾的最长无重复子串就是bca,然后再和之前的res取最大值。
+		 * 然后i指向后面的位置，按照同样思路计算。
+		 */
+		// 记录字符上一次出现的位置
+		int[] last = new int[128]; // 为什么是128?因为ASCII码有128个
+		for(int i = 0; i < 128; i++) {
+			last[i] = -1;
+		}
+		int n = s.length();
+
+		int res = 0;
+		int start = 0; // 窗口开始位置
+		for(int i = 0; i < n; i++) {
+			int index = s.charAt(i);//字符转成数字 ASCII表中的数字
+			start = Math.max(start, last[index] + 1);
+			res   = Math.max(res, i - start + 1);
+			last[index] = i;
+		}
+
+		return res;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/leetcode/editor/cn/[5]最长回文子串.java
+++ src/leetcode/editor/cn/[5]最长回文子串.java
@@ -24,9 +24,65 @@ public class 最长回文子串{
 	}
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public String longestPalindrome(String s) {
+	public String longestPalindrome(String s) {
+////		如果字符串为空返回空
+//		if (s == null || s.length() == 0) {
+//			return "";
+//		}
+////         保存起始位置，测试了用数组似乎能比全局变量稍快一点
+//		int[] range = new int[2];
+////		将字符串变成字符数组
+//		char[] str = s.toCharArray();
+//		for (int i = 0; i < s.length(); i++) {
+////             把回文看成中间的部分全是同一字符，左右部分相对称
+////             找到下一个与当前字符不同的字符
+//			i = findLongest(str, i, range);
+//		}
+//		return s.substring(range[0], range[1] + 1);
 
-    }
+
+//		动态规划
+			int n = s.length();
+			boolean[][] dp = new boolean[n][n];
+			String ans = "";
+			for (int l = 0; l < n; ++l) {
+				for (int i = 0; i + l < n; ++i) {
+					int j = i + l;
+					if (l == 0) {
+						dp[i][j] = true;
+					} else if (l == 1) {
+						dp[i][j] = (s.charAt(i) == s.charAt(j));
+					} else {
+						dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+					}
+					if (dp[i][j] && l + 1 > ans.length()) {
+						ans = s.substring(i, i + l + 1);
+					}
+				}
+			}
+			return ans;
+	}
+
+	public static int findLongest(char[] str, int low, int[] range) {
+//         查找中间部分
+		int high = low;
+		while (high < str.length - 1 && str[high + 1] == str[low]) {
+			high++;
+		}
+//         定位中间部分的最后一个字符
+		int ans = high;
+//         从中间向左右扩散
+		while (low > 0 && high < str.length - 1 && str[low - 1] == str[high + 1]) {
+			low--;
+			high++;
+		}
+//         记录最大长度
+		if (high - low > range[1] - range[0]) {
+			range[0] = low;
+			range[1] = high;
+		}
+		return ans;
+	}
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
