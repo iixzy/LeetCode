2cc89168955ad0332d6b41ef05fea9c8355b9765
wangsc <wsc4007@gmail.com>
2021-05-07 15:47:29 +1200

    2021年5月7日15:47

--- src/leetcode/editor/cn/[3]无重复字符的最长子串.java
+++ src/leetcode/editor/cn/[3]无重复字符的最长子串.java
@@ -88,22 +88,47 @@ class Solution {
 		 * 然后i指向后面的位置，按照同样思路计算。
 		 */
 		// 记录字符上一次出现的位置
-		int[] last = new int[128]; // 为什么是128?因为ASCII码有128个
-		for(int i = 0; i < 128; i++) {
-			last[i] = -1;
-		}
+//		int[] last = new int[128]; // 为什么是128?因为ASCII码有128个
+//		for(int i = 0; i < 128; i++) {
+//			last[i] = -1;
+//		}
+//		int n = s.length();
+//
+//		int res = 0;
+//		int start = 0; // 窗口开始位置
+//		for(int i = 0; i < n; i++) {
+//			int index = s.charAt(i);//字符转成数字 ASCII表中的数字
+//			start = Math.max(start, last[index] + 1);
+//			res   = Math.max(res, i - start + 1);
+//			last[index] = i;
+//		}
+//
+//		return res;
+
+		/*
+		* 第二个a start = Math.max(start, last[index] + 1) = Math.max(0, 0 + 1) = 1
+		* i = 3  res   = Math.max(res, i - start + 1) = Math.max(1, 3 - 1 + 1) = 3*/
+
+		// 哈希集合，记录每个字符是否出现过
+		Set<Character> occ = new HashSet<>();
 		int n = s.length();
-
-		int res = 0;
-		int start = 0; // 窗口开始位置
-		for(int i = 0; i < n; i++) {
-			int index = s.charAt(i);//字符转成数字 ASCII表中的数字
-			start = Math.max(start, last[index] + 1);
-			res   = Math.max(res, i - start + 1);
-			last[index] = i;
+		// 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+		int rk = -1, ans = 0;
+		for (int i = 0; i < n; i++) {
+			if (i != 0) {
+				// 左指针向右移动一格，移除一个字符
+				occ.remove(s.charAt(i - 1));
+			}
+			while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+				// 不断地移动右指针
+				occ.add(s.charAt(rk + 1));
+				++rk;
+			}
+			// 第 i 到 rk 个字符是一个极长的无重复字符子串
+			ans = Math.max(ans, rk - i + 1);
 		}
 
-		return res;
+		return ans;
 
     }
 }
